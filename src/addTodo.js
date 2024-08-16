0bf7007826e700b3e3027f7becc48c32bd098654
ics-kitagawa <kitagawa@ics-web.jp>
2024-08-16 15:01:41 +0900

    PR指摘修正：completedの引数を追加

--- src/addTodo.js
+++ src/addTodo.js
@@ -3,9 +3,10 @@
  * @param {Array} todos 既存のTodoリスト
  * @param {string} title TODOのタイトル
  * @param {string} dueDate TODOの期限
+ * @param {boolean} completed TODOが完了済みかどうか。指定しない場合はfalse。
  * @returns {Array} 新しいTodoを追加したTodoリスト
  */
-export const addTodo = (todos, title, dueDate) => {
-  const newTodo = { title, dueDate, completed: false };
+export const addTodo = (todos, title, dueDate, completed = false) => {
+  const newTodo = { title, dueDate, completed };
   return [...todos, newTodo];
 };
